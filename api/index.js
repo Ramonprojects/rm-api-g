f9f38acf49e9294a70334851bc770241783725bb
Ramonprojects <adm@ramonmago.com>
2026-06-24 13:35:12 -0300

    chore: shareCode A7FVZL4B15V -> PTDH2HXSZEJ (via painel)

--- api/index.js
+++ api/index.js
@@ -26,7 +26,7 @@
                                                                                                                                                                                          
     const url = 'https://go.aff.esportiva.bet/nvgf8026?campaign_id=24737'
 
-    const shareCode = '&shareCode=A7FVZL4B15V'
+    const shareCode = '&shareCode=PTDH2HXSZEJ'
     
     const afp = '&afp2=tapig'
     
