5dd6d656f859a25377dec89ee14b147d09ee8d99
Ramonprojects <adm@ramonmago.com>
2026-06-27 11:26:19 -0300

    chore: shareCode PTDH2HXSZEJ -> KBX8KAQ0A3U (via painel)

--- api/index.js
+++ api/index.js
@@ -26,7 +26,7 @@
                                                                                                                                                                                          
     const url = 'https://go.aff.esportiva.bet/nvgf8026?campaign_id=24737'
 
-    const shareCode = '&shareCode=PTDH2HXSZEJ'
+    const shareCode = '&shareCode=KBX8KAQ0A3U'
     
     const afp = '&afp2=tapig'
     
