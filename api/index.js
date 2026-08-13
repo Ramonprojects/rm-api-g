184de01b10ab060f082d9f051c7ff27dc248b4e1
Ramonprojects <adm@ramonmago.com>
2026-08-13 10:53:16 -0300

    chore: shareCode ADK5SHIB9S1 -> 8NT8ZQU6PYD (via painel)

--- api/index.js
+++ api/index.js
@@ -26,7 +26,7 @@
                                                                                                                                                                                          
     const url = 'https://go.aff.esportiva.bet/nvgf8026?campaign_id=24737'
 
-    const shareCode = '&shareCode=ADK5SHIB9S1'
+    const shareCode = '&shareCode=8NT8ZQU6PYD'
     
     const afp = '&afp2=tapig'
     
